f61565d1b7d404d6607a04dc8a4189fed319f4a3
Eran Zimbler <eran.zimbler@cloudofthings.com>
2016-10-08 09:36:05 +0300

    updating to script to be a bit easier to use

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
-FROM alpine:3.2
+FROM alpine:3.4
 MAINTAINER Eran Zimbler <eran@zimbler.net>
-
-
-RUN apk --update add python libxml2-dev libxslt-dev
-RUN apk --update add --virtual build-dependencies py-pip python-dev build-base  && pip install virtualenv
-  
-
 
 WORKDIR /src
 COPY . /src
 
-RUN ln -s /usr/include/libxml2/libxml /usr/include/libxml
-RUN virtualenv env && env/bin/pip install -r ./requirements.txt
-RUN apk del build-dependencies && rm -rf /var/cache/apk/*
+RUN apk --update add libxml2-dev libxslt-dev && \ 
+    apk --update add --virtual build-dependencies python3 python3-dev build-base && \
+    ln -s /usr/include/libxml2/libxml /usr/include/libxml && \
+    python3 -m ensurepip && pip3 install --upgrade pip && \
+    pip install -r ./requirements.txt && \
+    apk del build-dependencies && rm -rf /var/cache/apk/*
 
 CMD ["env/bin/python", "add_to_lib.py"]
--- add_to_lib.py
+++ add_to_lib.py
@@ -1,62 +1,64 @@
-import requests
-import os
-from lxml import html
+""" a simple script to get the packet publishing free book of the day into your account """
+import argparse
+import json
 import logging
+import os
 import sys
 
-LOG_LEVEL = 'WARNING'
+import requests
+from lxml import html
+import getpass
 
-DEBUG = False
-BOOK_FORMATS = [
-    # 'epub',
-    # 'mobi',
-    'pdf'
-]
-if not BOOK_FORMATS:
-    print "Error: Book format is not selected (e.g. pdf)."
-    sys.exit()
-
+CONFIG = None
+HEADERS = {'User-Agent': 'grab_packet.py free claim script'}
 BASE_URL = 'https://www.packtpub.com'
-DOWNLOAD_NEW = True
-BOOKS_DIR = os.path.dirname(os.path.realpath(__file__)) +"/books"
 
-if DEBUG:
-    import pdb
-    LOG_LEVEL = 'DEBUG'
+def setup(args):
+    log_level = 'WARNING'
+    if args.debug:
+        # import pdb
+        log_level = 'DEBUG'
+    log_format = '%(asctime)-15s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=log_format, level=log_level)
+    logging.debug('Setup logging configuration')
+    config = {}
+    if args.config:
+        logging.debug('loading config file')
+        try:
+            with open(args.config) as configfile:
+                config = json.load(configfile)
+        except IOError:
+            logging.critical("Failed to load the file")
+            sys.exit(1)
+        except ValueError:
+            logging.critical("Failed to read the config file probably not proper json")
+            sys.exit(1)
+        except Exception, e:
+            raise e
+        return config
+    else:
+        logging.debug('no config file specified')
+    for key in os.environ:
+        if "CONFIG_" in key:
+            config[key.replace('CONFIG_').lower()] = os.environ.get(key)
+    return config
+
+def set_arg_in_config(args,name):
+    if hasattr(args,name) and getattr(args,name) is not None:
+        CONFIG[name] = getattr(args,name)
 
 
-FORMAT = '%(asctime)-15s %(message)s'
-logging.basicConfig(format=FORMAT, level=LOG_LEVEL)
-
-
-def break_point():
-    if DEBUG:
-        pdb.set_trace()
-
-
-def error(msg):
-    logging.error(msg)
-    sys.exit(1)
-
-
-def main():
-    session = requests.session()
-    logging.info('setting session')
+def login(session):
     login_data = {
-        'email': os.environ.get('PACKT_EMAIL'),
-        'password': os.environ.get('PACKT_PASSWORD'),
+        'email': CONFIG.get('email'),
+        'password': CONFIG.get('password'),
         'op': 'Login',
         'form_id': 'packt_user_login_form',
         'form_build_id': None,
     }
     logging.debug('setting login data without form_build_id ' + str(login_data))
-
-    if login_data['email'] is None or login_data['password'] is None:
-        error('You must provide credentials')
-
     free_learning_url = 'https://www.packtpub.com/packt/offers/free-learning'
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    r = session.get(free_learning_url,headers=headers)
+    r = session.get(free_learning_url, headers=HEADERS)
     tree = html.fromstring(r.text)
     form_build_id_elem = tree.xpath('//input[@name="form_build_id"]')
     book_link_elem = tree.xpath('//a[@class="twelve-days-claim"]')
@@ -67,49 +69,89 @@ def main():
         login_data['form_build_id'] = form_build_id_elem[0].get('id')
         logging.debug('setting form_build_id in login_data to ' + login_data['form_build_id'])
     else:
-        error('Failed to set form_build_id')
+        logging.critical('Failed to set form_build_id')
 
-    r = session.post(free_learning_url, headers={'content-type': 'application/x-www-form-urlencoded','User-Agent': 'My User Agent 1.0'}, data=login_data)
+    headers = HEADERS
+    headers['content-type']= 'application/x-www-form-urlencoded'
+    r = session.post(free_learning_url, headers=headers, data=login_data)
     logging.info('Logged in to packtpub.com')
     tree = html.fromstring(r.text)
     elem = tree.xpath('//div[contains(@class,"error")]')
 
     # break_point()
     if len(elem) > 0 and "Sorry, you entered an invalid email address and password combination." in elem[0].text:
-        error('Failed to login please check your credentials')
+        logging.critical('Failed to login please check your credentials')
 
-    r = session.get(book_url,headers=headers)
+    return book_url
+
+def claim_book(session,book_url):
+    r = session.get(book_url,headers=HEADERS)
     tree = html.fromstring(r.text)
     book_elem = tree.xpath('//div[contains(@class,"product-line")]')
     book_name = book_elem[0].get('title')
-    logging.debug('book name = ' + book_name)
-    book_name.replace('"', '\\"')
+    book_id = book_elem[0].get('nid')
+    logging.debug('book_name={}, book_id={}'.format(book_name,book_id))
+    return (book_name,book_id)
 
-    download_log = os.getcwd() +"/books.log"
-    if not os.path.isdir(BOOKS_DIR):
-        os.mkdir(BOOKS_DIR)
-        logging.debug('Creating directory: '+ BOOKS_DIR)
-    logging.debug('Books directory: '+ BOOKS_DIR)
-    os.chdir(BOOKS_DIR)
+def format_book_name(name,id,suffix):
+    ret = '({}){}.{}'.format(id,name,suffix)
+    ret = ret.replace('"', '\\"').replace('/','_')
+    return ret
 
-    # Downloading book in all formats
-    for bf in BOOK_FORMATS:
-        book_id = book_elem[0].get('nid')
+def download_book(session,book_name,book_id):
+    dest = CONFIG.get('dest','./books')
+    if not os.path.isdir(dest):
+        os.mkdir(dest)
+        logging.info('creating destination for downloaded books')
+    else:
+        logging.info('using {} as destination'.format(dest))
+    for bf in CONFIG.get('format',[]):
         book_url = BASE_URL + '/ebook_download/' + book_id + '/' + bf
-        fname = '('+ book_id + ')' + book_name  + '.' + bf
-        fname = fname.replace('/','_')
-        if DOWNLOAD_NEW and os.path.exists(BOOKS_DIR +'/'+ fname):
-            logging.debug('File '+ fname +' exists. Skip download')
+        filename = format_book_name(book_name,book_id,bf)
+        filepath = os.path.join(dest,filename)
+        if os.path.exists(filepath):
+            logging.info('File already exists skipping format')
             continue
-        logging.info('Getting book ' + fname)
-        with open(fname, 'wb') as handle:
-            response = session.get(book_url, stream=True)
+        with open(filepath, 'wb') as book_file:
+            response = session.get(book_url, stream=True,headers=HEADERS)
             for chunk in response:
-                handle.write(chunk)
-        with open(download_log, 'a') as f:
-            f.write("Download id:%s, format:%s, name:%s\n" % (book_id, bf, book_name))
+                book_file.write(chunk)
+            logging.info("downloaded {} into {}".format(book_name, filepath))
 
-    # end of downloading files
+
+def get_book():
+    session = requests.session()
+    logging.info('setting session')
+    if CONFIG['email'] is None or CONFIG['password'] is None:
+        logging.critical('You must provide credentials')
+    book_url = login(session)
+    book_elem = claim_book(session,book_url)
+    if not CONFIG.get('disable_download',False):
+        download_book(session,book_elem[0],book_elem[1])
+    
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-c', '--config', help='configuration file to load')
+    parser.add_argument('-D', '--debug', help='enable debug mode', action='store_true')
+    parser.add_argument('-e', '--email', help='your packet account email')
+    parser.add_argument('-p', '--password', help='your packet account email')
+    parser.add_argument('-f','--format',action='append',help='book format, can be used multiple times')
+    parser.add_argument('--disable-download',action='store_true',help='Dsiable downloading of the book')
+    args = parser.parse_args()
+    global CONFIG
+    CONFIG = setup(args)
+    set_arg_in_config(args,'password')
+    if CONFIG.get('password') is None:
+        CONFIG['password'] = getpass.getpass()
+    set_arg_in_config(args,'email')
+    set_arg_in_config(args,'format')
+    set_arg_in_config(args,'disable_download')
+    
+
+    get_book()
 
 if __name__ == '__main__':
     main()
